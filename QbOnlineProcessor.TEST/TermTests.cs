77949ee499ee6dc2509a458daca2ab40555d35cd
Greg Prieto <Greg.Prieto@ncsecu.org>
2022-05-14 11:25:12 -0400

    Term tests added

---
+++ QbOnlineProcessor.TEST/TermTests.cs
@@ -0,0 +1,109 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QbModels.QBO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace QbModels.QBOProcessor.TEST
+{
+    [TestClass]
+    public class TestTermModels
+    {
+        readonly string testName = "IMS Term";
+
+        [TestMethod]
+        public async Task Step_1_QBOTermQueryTest()
+        {
+            #region Setting access token
+            TestAccessToken accessToken = new();
+            await accessToken.AccessTokenTest();
+            #endregion
+
+            using QBOProcessor qboe = new();
+
+            #region Getting Terms
+            if (string.IsNullOrEmpty(qboe.AccessToken.AccessToken)) Assert.Fail("Token not valid.");
+            
+            HttpResponseMessage getRs = await qboe.QBOGet(QueryRq.QueryParameter(qboe.ClientInfo.RealmId, "select * from Term"));
+            if (!getRs.IsSuccessStatusCode) Assert.Fail($"QBOGet failed: {await getRs.Content.ReadAsStringAsync()}");
+
+            string qryRsStr = await getRs.Content.ReadAsStringAsync();
+            TermOnlineRs qryRs = new(qryRsStr);
+            Assert.IsNull(qryRs.ParseError, $"Term query parsing error: {qryRs.ParseError}");
+            Assert.AreNotEqual(0, qryRs.TotalTerms, "No Terms found.");
+            #endregion
+        }
+
+        [TestMethod]
+        public async Task Step_2_QBOTermAddTest()
+        {
+            #region Setting access token
+            TestAccessToken accessToken = new();
+            await accessToken.AccessTokenTest();
+            #endregion
+
+            using QBOProcessor qboe = new();
+
+            #region Getting Term
+            if (string.IsNullOrEmpty(qboe.AccessToken.AccessToken)) Assert.Fail("Token not valid.");
+            
+            HttpResponseMessage getRs = await qboe.QBOGet(QueryRq.QueryParameter(qboe.ClientInfo.RealmId, $"select * from Term where Name='{testName}'"));
+            
+            TermOnlineRs termRs = new(await getRs.Content.ReadAsStringAsync());
+            #endregion
+
+            #region Adding Term
+            if (termRs.TotalTerms > 0) Assert.Inconclusive($"{testName} already exists.");
+
+            TermAddRq addRq = new();
+            addRq.Name = testName;
+            addRq.DueDays = 0;
+            if (!addRq.IsEntityValid()) Assert.Fail($"addRq is invalid: {addRq.GetErrorsAsString()}");
+
+            HttpResponseMessage postRs = await qboe.QBOPost(addRq.ApiParameter(qboe.ClientInfo.RealmId), addRq);
+            if (!postRs.IsSuccessStatusCode) Assert.Fail($"QBOPost failed: {await postRs.Content.ReadAsStringAsync()}");
+
+            TermOnlineRs addRs = new(await postRs.Content.ReadAsStringAsync());
+            Assert.AreEqual(testName, addRs.Terms?[0]?.Name);
+            #endregion
+        }
+
+        [TestMethod]
+        public async Task Step_3_QBOTermModTest()
+        {
+            #region Setting access token
+            TestAccessToken accessToken = new();
+            await accessToken.AccessTokenTest();
+            #endregion
+
+            using QBOProcessor qboe = new();
+
+            #region Getting Term
+            if (string.IsNullOrEmpty(qboe.AccessToken.AccessToken)) Assert.Fail("Token not valid.");
+            
+            HttpResponseMessage getRs = await qboe.QBOGet(QueryRq.QueryParameter(qboe.ClientInfo.RealmId, $"select * from Term"));
+            
+            TermOnlineRs termRs = new(await getRs.Content.ReadAsStringAsync());
+            #endregion
+
+            #region Updating Term
+            if (termRs.TotalTerms <= 0) Assert.Fail($"No {testName} to update.");
+
+            TermDto term = termRs.Terms.FirstOrDefault(a => a.Name.Equals(testName));
+            if (term == null) Assert.Fail($"{testName} does not exist.");
+
+            TermModRq modRq = new();
+            modRq.CopyDto(term);
+            modRq.sparse = "true";
+            modRq.DueDays = (term.DueDays ?? 0) + 30;
+            if (!modRq.IsEntityValid()) Assert.Fail($"modRq is invalid: {modRq.GetErrorsAsString()}");
+
+            HttpResponseMessage postRs = await qboe.QBOPost(modRq.ApiParameter(qboe.ClientInfo.RealmId), modRq);
+            if (!postRs.IsSuccessStatusCode) Assert.Fail($"QBOPost failed: {await postRs.Content.ReadAsStringAsync()}");
+
+            TermOnlineRs modRs = new(await postRs.Content.ReadAsStringAsync());
+            Assert.AreNotEqual(term.DueDays, modRs.Terms?[0]?.DueDays);
+            #endregion
+        }
+    }
+}
